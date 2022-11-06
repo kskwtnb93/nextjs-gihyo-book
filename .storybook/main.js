f5a26e603d35bd8f9ce746f200c23db793011884
keisuke watanabe <kskwtnb93@gmail.com>
2022-11-07 02:28:44 +0900

    settings of storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,6 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
+const path = require('path')
+
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
@@ -13,4 +16,21 @@ module.exports = {
     "builder": "@storybook/builder-webpack5"
   },
   staticDirs: ['public'],
+  babel: async options => ({
+	...options,
+	plugins: [
+		'@babel/plugin-proposal-class-properties',
+		'@babel/plugin-proposal-private-methods',
+		'@babel/plugin-proposal-private-property-in-object',
+	],
+  }),
+  webpackFinal: async (config) => {
+	config.resolve.plugins = [
+		new TsconfigPathsPlugin({
+			configFile: path.resolve(__dirname, '../tsconfig.json')
+		}),
+	];
+
+	return config
+  },
 }
