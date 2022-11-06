dc99a1847f99eaca332a4010b795d7973962c502
keisuke watanabe <kskwtnb93@gmail.com>
2022-11-06 21:10:13 +0900

    settings of styled-components

---
+++ src/pages/_document.tsx
@@ -0,0 +1,34 @@
+// 以下 Styled Components 公式 GitHub からのコピペ
+// → https://github.com/vercel/next.js/blob/canary/examples/with-styled-components-babel/pages/_document.tsx
+import Document, { DocumentContext, DocumentInitialProps } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
+
+export default class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      }
+    } finally {
+      sheet.seal()
+    }
+  }
+}
